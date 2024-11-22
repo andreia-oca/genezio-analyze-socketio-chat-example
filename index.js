dfb366f770097bc0c3e9bc4e3b39354dee294eb7
bogdanripa <bogdanripa@gmail.com>
2024-11-22 13:57:24 +0200

    port 8080

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ io.on('connection', async (socket) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
